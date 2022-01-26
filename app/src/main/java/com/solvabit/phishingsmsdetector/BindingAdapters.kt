43bd09bdbda4e63bcf77fd04a06d07511e784373
Chandresh Singh <chaharchandresh@gmail.com>
2022-01-26 20:47:05 +0530

    Changed Home UI

--- app/src/main/java/com/solvabit/phishingsmsdetector/BindingAdapters.kt
+++ app/src/main/java/com/solvabit/phishingsmsdetector/BindingAdapters.kt
@@ -35,7 +35,6 @@ fun getColor(string: String): Int {
 @BindingAdapter("bindTextColor")
 fun bindTextColor(textView: TextView, int: Int?) {
     int?.let {
-        Log.i(TAG, "bindTextColor: $it")
         when(it) {
             0 -> {
                 textView.setTextColor(Color.parseColor("#000000"))
@@ -43,6 +42,7 @@ fun bindTextColor(textView: TextView, int: Int?) {
             }
             else -> {
                 textView.setTextColor(Color.parseColor("#99000000"))
+                textView.setTypeface(null, Typeface.NORMAL)
             }
         }
     }
@@ -55,6 +55,21 @@ fun bindImageThumbnail(imgView: ImageView, thumbnails: Thumbnails?) {
         Glide.with(imgView.context)
             .load(imgUri)
             .into(imgView)
+    }
+}
+
+@BindingAdapter("bindFormatDate")
+fun bindDate(textView: TextView, date: Long?) {
+    date?.let {
+        val messageDate = Date(date).toString()
+        val todayDate = Calendar.getInstance().time.toString()
+
+        if(messageDate.substring(0, 10) == todayDate.substring(0, 10))
+            textView.text = messageDate.substring(11, 16)
+        else
+            textView.text = messageDate.substring(4, 10)
+
+
     }
 }
 
--- app/src/main/java/com/solvabit/phishingsmsdetector/screens/details/MessageDetailsFragment.kt
+++ app/src/main/java/com/solvabit/phishingsmsdetector/screens/details/MessageDetailsFragment.kt
@@ -3,9 +3,6 @@ package com.solvabit.phishingsmsdetector.screens.details
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,6 +15,7 @@ import android.content.Context
 
 import android.content.Intent
 import android.net.Uri
+import android.view.*
 
 
 private const val TAG = "MessageDetailsFragment"
